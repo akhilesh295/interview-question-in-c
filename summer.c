2c15341c32ef85c580a447e6cdcf9a65adc7ba00
akhilesh295 <metreakhileshyadav295@gmail.com>
2026-02-02 16:00:53 +0530

    Add files via upload

---
+++ summer.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main()
+{
+int temp;
+int summer;
+printf("enter the temp\n");
+scanf("%d",&temp);
+printf("enter 1 for summer \t 0 for not summer\n");
+scanf("%d",&summer);
+if(summer)
+{
+    if(temp>=60&& temp<=100)
+{
+    printf("true");
+}
+else
+{
+    printf("false");
+}
+}
+else
+{
+    if(temp>=60 && temp<=0)
+    {
+        printf("true");
+    }
+    else
+    {
+        printf("false");
+    }
+}
+    
+}
